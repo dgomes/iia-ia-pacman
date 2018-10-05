2e2b3f278d5698608b1b094e94335fa9ccc50b2c
Diogo Gomes <dgomes@av.it.pt>
2018-10-05 22:31:56 +0100

    changing libs

--- game.py
+++ game.py
@@ -9,21 +9,24 @@ class Game:
         self.running = False
         self.step = 0
         logger.info("Game()")
+        self.player = None
 
     def reset_world(self):
         logger.debug("Reset world")
 
     def join(self, name, ws):
         logger.debug("Join {}".format(name))
+        self.player = ws
 
-    def next_frame(self):
+    async def next_frame(self):
         if not self.running:
             self.running = True
         self.step += 1
-        logger.debug("Step {}", self.step) 
+        logger.debug("Step {}".format(self.step))
+        await self.send(self.player, {"Step": self.step}) 
 
-    def send(self, ws, *args):
+    async def send(self, ws, *args):
         msg = json.dumps([args])
-        ws.send_str(msg)
+        await ws.send_str(msg)
 
 
--- server.py
+++ server.py
@@ -7,13 +7,29 @@ from game import Game
 
 GAME_SPEED = 1
 
-_LOGGER = logging.getLogger(__name__)
-_LOGGER.setLevel(logging.DEBUG)
+async def handle(request):
+    """
+    Serves files for the GUI
+    """
+    ALLOWED_FILES = ["map", "pacman.png", "ghost.png"]
+    name = request.match_info.get('name')
+    if name in ALLOWED_FILES:
+        try:
+            with open(name, 'rb') as index:
+                return web.Response(body=index.read(), content_type='image/png')
+        except FileNotFoundError:
+            pass
+    return web.Response(status=404)
 
 async def wshandler(request):
-    _LOGGER.debug("Connected")
+    """
+    Handle agents commands
+    """
+    logging.debug("New Agent connected")
+    
     app = request.app
     game = app["game"]
+   
     ws = web.WebSocketResponse()
     await ws.prepare(request)
 
@@ -21,14 +37,14 @@ async def wshandler(request):
     while True:
         msg = await ws.receive()
         if msg.type == web.WSMsgType.TEXT:
-            _LOGGER.debug("Got message %s" % msg.data)
+            logging.debug("Got message %s" % msg.data)
 
             data = json.loads(msg.data)
             if data["cmd"] == "join":
                 if not game.running:
                     game.reset_world()
 
-                    _LOGGER.debug("Starting game loop")
+                    logging.debug("Starting game loop")
                     asyncio.ensure_future(game_loop(game))
 
                 game.join(player,ws)
@@ -39,29 +55,29 @@ async def wshandler(request):
     if player:
         game.player_disconnected(player)
 
-    _LOGGER.debug("Closed connection")
+    logging.debug("Agent disconnected")
     return ws
 
 async def game_loop(game):
-    _LOGGER.info("Starting game")
+    logging.info("Starting game")
     while True:
-        game.next_frame()
+        await game.next_frame()
         if not game.running:
             break
         await asyncio.sleep(1./GAME_SPEED)
-    _LOGGER.info("Stopping game")
+    logging.info("Stopping game")
 
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
 
+    event_loop = asyncio.get_event_loop()
 
-event_loop = asyncio.get_event_loop()
-event_loop.set_debug(True)
+    app = web.Application()
 
-app = web.Application()
+    app["game"] = Game()
 
-app["game"] = Game()
+    app.router.add_route('GET', '/connect', wshandler)
+    app.router.add_route('GET', '/', handle)
 
-app.router.add_route('GET', '/connect', wshandler)
-#app.router.add_route('GET', '/', handle)
-
-port = int(os.environ.get('PORT', 8000))
-web.run_app(app, port=port)
+    port = int(os.environ.get('PORT', 8000))
+    web.run_app(app, port=port)
