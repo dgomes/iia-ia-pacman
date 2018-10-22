5c5e39d213b9b8c00600ebb374d5734519f2905a
Diogo Gomes <dgomes@ua.pt>
2018-10-22 17:58:18 +0100

    work in progress

--- server.py
+++ server.py
@@ -8,50 +8,62 @@ import os.path
 from game import Game
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger('websockets')
-logger.setLevel(logging.WARN)
+wslogger = logging.getLogger('websockets')
+wslogger.setLevel(logging.WARN)
 
 class Game_server:
     def __init__(self, mapfile, ghosts, lives):
         self.game = Game(mapfile, ghosts, lives) 
         self.clients = set()
+        self.hotseat = asyncio.Condition() 
         self.highscores = [] 
         if os.path.isfile(mapfile+".score"):
             with open(mapfile+".score", 'r') as infile:
                 self.highscores = json.load(infile)
-        self.current_player_name = None
+        self.current_player = None, None 
 
     async def incomming_handler(self, websocket, path):
         self.clients.add(websocket)
-    
         try:
             async for message in websocket:
                 data = json.loads(message)
+                logging.debug(data)
                 if data["cmd"] == "join":
                     map_info = self.game.info()
                     await asyncio.wait([websocket.send(map_info)])
                     
-                    if path == "/player" and not self.current_player_name:
-                        self.current_player = websocket
-                        self.current_player_name = data["name"]
-                        self.game.start(self.current_player_name) 
+                    if path == "/player":
+                        if self.game.running:
+                            logging.debug("Wait for hotseat")
+                            await self.hotseat.wait()
 
-                if path == "/player" and data["cmd"] == "key":
+                        if not self.game.running:
+                            if self.current_player != (None,None): 
+                                self.hotseat.release()
+                            logging.debug("Start Game")
+                            self.current_player = data["name"], websocket
+                            self.game.start(self.current_player[0])
+                            await self.hotseat.acquire()
+                        
+                if data["cmd"] == "key" and path == "/player":
                     self.game.keypress(data["key"][0])
 
         except websockets.exceptions.ConnectionClosed as c:
             logging.info("Client disconnected")
         finally:
-            self.clients.remove(websocket)
+            if websocket in self.clients:
+                logging.debug("remove client")
+                self.clients.remove(websocket)
             if not self.clients and self.game.running:
-                logger.info("close the game")
+                logging.info("close the game")
                 self.game.stop()
+            self.hotseat.notify()    
 
     async def state_broadcast_handler(self, websocket, path):
         while path == "/viewer" or not self.game.running:
             await asyncio.sleep(.1)
         while self.game.running:
-            if path == "/player" and self.current_player == websocket:
+            if path == "/player" and self.current_player[1] == websocket:
                 await self.game.next_frame()
                 if self.clients:       # asyncio.wait doesn't accept an empty list
                     await asyncio.wait([client.send(self.game.state) for client in self.clients])
@@ -59,12 +71,11 @@ class Game_server:
         #update highscores
         if path == "/player":
             logging.debug("Save highscores")
-            self.highscores.append((self.current_player_name, self.game.score))
+            self.highscores.append((self.current_player[0], self.game.score))
             self.highscores = sorted(self.highscores, key=lambda s: -1*s[1])[:10]
     
             with open(self.game.map._filename+".score", 'w') as outfile:
                 json.dump(self.highscores, outfile)
-            return
 
 
 async def client_handler(websocket, path, game):
